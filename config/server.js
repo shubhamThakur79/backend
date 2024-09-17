8513ed971db4e1d1b49ebc7c5e8b1eab48117f07
Shubham Thakur <shubham.20102005.bca22@engg.hgpi.in>
2024-09-17 22:26:55 +0530

    Initial commit

--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+  enabled: true,
+  origin: env('CORS_ORIGIN', '*'), // Or specify your frontend URL like 'http://localhost:3000'
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  credentials: true,
 });
